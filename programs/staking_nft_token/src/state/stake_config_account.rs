6f7d64bd034f80c48fd411e489da7b19ad184cba
solanagithubraunit15 <raunitjaiswal510@gmail.com>
2025-06-04 20:01:05 +0000

    adding state

---
+++ programs/staking_nft_token/src/state/stake_config_account.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace,Debug)]
+pub struct StakeConfigAccount{
+    pub points_per_nft_stake: u8,
+    pub points_per_sol_stake: u8,
+    pub points_per_spl_stake: u8,
+    pub min_freeze_period: u32,
+    pub reward_bump: u8,
+    pub bump: u8 
+}
